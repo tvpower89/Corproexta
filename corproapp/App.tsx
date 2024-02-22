65ec932b49a88b033d0f5fdcab06bb217644d0e4
tvpower89 <tvpower89@hotmail.com>
2024-02-22 12:03:58 -0800

    app

--- corproapp/App.tsx
+++ corproapp/App.tsx
@@ -1,39 +1,15 @@
-import React, {useState} from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import InformationInputPage from './InformationInputPage';
-import {Alert} from "react-native";
+import React, { useState } from 'react';
+import { View, Text, Button, Alert } from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import InformationInputPage from './InformationInputPage'; // Import your new component for information input
+import QRScannerScreen from "./QRScannerScreen.tsx";
+import {NavigationContainer} from "@react-navigation/native";
 
-const Stack = createStackNavigator();
-
-const QRScannerScreen = () => {
-    const [scanned, setScanned] = useState(false);
-    const [showInputPage, setShowInputPage] = useState(false); // State to show the input page
-
-    const handleQRCodeScanned = ({ data }: { data: string }) => {
-        setScanned(true);
-        setShowInputPage(true); // Show the input page after scanning QR code
-        //saveDataOffline(data);
-        Alert.alert('QR Code Scanned', `Data: ${data}`, [{ text: 'OK' }]);
-    };
-
-    /* const saveDataOffline = async (data: string) => {
-         try {
-             const existingData = await AsyncStorage.getItem('offlineData');
-             const newData = existingData ? JSON.parse(existingData) : [];
-             newData.push(data);
-             await AsyncStorage.setItem('offlineData', JSON.stringify(newData));
-         } catch (error) {
-             console.error('Error saving data offline:', error);
-         }
-     };*/
 const App = () => {
     return (
         <NavigationContainer>
-            <Stack.Navigator initialRouteName="QRScanner">
-                <Stack.Screen name="QRScanner" component={QRScannerScreen} />
-                <Stack.Screen name="InformationInput" component={InformationInputPage} />
-            </Stack.Navigator>
+            <QRScannerScreen />
         </NavigationContainer>
     );
 };
